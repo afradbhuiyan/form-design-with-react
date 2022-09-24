9395e134a34d5666c2dfa5ffd61af0bde7c01652
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2022-09-24 18:27:41 +0600

    add a login link in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,22 @@
+import { NavLink } from "react-router-dom"
+
 function Home() {
   return (
      <>
-         <section>
-            <div className="container">
-               <div>Home page</div>
+         <section className="w-full min-h-[calc(100vh-80px)] flex">
+            <div className="container py-4 my-auto">
+               <div className="w-1/2 flex flex-col space-y-4 items-center mx-auto bg-white px-4 py-8 rounded-sm">
+                     <h1 className="text-2xl text-center font-bold text-gray-700 tracking-wide">
+                        Login page design using ReactJs and TwailwindCSS Styled Components
+                     </h1>
+
+                     <NavLink className="inline-block text-primary-500 font-medium capitalize" to="/login">
+                        Go to login page
+                     </NavLink>
+               </div>
             </div>
          </section>
+         
      </>
   )
 }
